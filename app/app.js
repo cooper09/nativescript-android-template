a07eeee1c7f02474d612cfb1495d6f800ff8785c
cooper09 <cooperatkmba@gmail.com>
2020-10-21 16:32:50 -0400

    working stuff

--- app/app.js
+++ app/app.js
@@ -1,16 +1,10 @@
 
 //app.js
 const app = require('tns-core-modules/application');
-const Label = require('tns-core-modules/ui/label').Label;
-const Button = require('tns-core-modules/ui/button').Button;
-
-const StackLayout = require('tns-core-modules/ui/layouts/stack-layout').StackLayout;
 
 app.run({       //Navigation Entry
 
-
         moduleName: 'root'
         
-
 });
 
