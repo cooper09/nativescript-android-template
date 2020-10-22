9f512f2c29ad0d59a267758f13177f5a11af6259
cooper09 <cooperatkmba@gmail.com>
2020-10-22 11:25:27 -0400

    add databinding

--- app/root.js
+++ app/root.js
@@ -1,3 +1,18 @@
+const { Observable}  = require('tns-core-modules/data/observable')
+const model = new Observable();
+
 exports.onTap = () => {
-    alert("Bind ME, baby!!!")
-} 
+    console.log("Bind ME, baby: ", model );
+    model.set("data","Hurray it worked!")
+    finishUp();
+} //end onTap
+
+exports.onLoaded = args => {
+    const page = args.object;
+    model.set("data", "please click button")
+    page.bindingContext = model;
+}  //end onLoaded
+
+const finishUp = () =>{
+    alert("OK, we're all done here...")
+}//end finishUp
