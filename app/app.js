5160a41dda9a2ffdd3dafc0e87208a5404cda402
cooper09 <cooperatkmba@gmail.com>
2020-10-23 10:32:40 -0400

    added naivebayes

--- app/app.js
+++ app/app.js
@@ -2,6 +2,10 @@
 //app.js
 const app = require('tns-core-modules/application');
 
+//let uuid = require('node-uuid');
+//console.log("uuid.v1: " + uuid.v1());//
+let bayes = require('ml-naivebayes');
+
 app.run({       //Navigation Entry
 
         moduleName: 'root'
--- app/root.js
+++ app/root.js
@@ -1,10 +1,14 @@
+const { CubicBezierAnimationCurve } = require('@nativescript/core/ui/animation');
 const { Observable}  = require('tns-core-modules/data/observable')
 const model = new Observable();
+
+let bayes = require('ml-naivebayes');
 
 exports.onTap = () => {
     console.log("Bind ME, baby: ", model );
     model.set("data","Hurray it worked!")
-    finishUp();
+    //finishUp();
+    runML();
 } //end onTap
 
 exports.onLoaded = args => {
@@ -14,5 +18,12 @@ exports.onLoaded = args => {
 }  //end onLoaded
 
 const finishUp = () =>{
-    alert("OK, we're all done here...")
+    alert("OK, we're all done here...");
+   // const brain = new brain();
+
 }//end finishUp
+
+const runML = () =>{
+    alert("Now lets apply our brain...");
+  //  const brain = new brain();  
+}
